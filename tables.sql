ad49cb115b3cbe24b1d2a29ce61fd8f231102bba
Ibrahim Noune <ibrahim.noune@etud.univ-angers.fr>
2019-04-05 14:54:54 +0200

    correction de l'insert dans la table categorie

--- tables.sql
+++ tables.sql
@@ -8,15 +8,15 @@ CREATE TABLE `CATEGORIE` (
    
 );
 
-insert into  values('SENIORS');
-insert into categorie values('U17');
-insert into categorie values('U15');
-insert into categorie values('U14-F');
-insert into categorie values('U13');
-insert into categorie values('U11-F');
-insert into categorie values('U11');
-insert into categorie values('U9');
-insert into categorie values('U7');
+insert into CATEGORIE(IDCategorie) values('SENIORS');
+insert into CATEGORIE(IDCategorie) values('U17');
+insert into CATEGORIE(IDCategorie) values('U15');
+insert into CATEGORIE(IDCategorie) values('U14-F');
+insert into CATEGORIE(IDCategorie) values('U13');
+insert into CATEGORIE(IDCategorie) values('U11-F');
+insert into CATEGORIE(IDCategorie) values('U11');
+insert into CATEGORIE(IDCategorie) values('U9');
+insert into CATEGORIE(IDCategorie) values('U7');
 
 
 
