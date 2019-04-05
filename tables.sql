97314a474f0e311ecceefe78d287159f88183746
Ibrahim Noune <ibrahim.noune@etud.univ-angers.fr>
2019-04-05 14:19:55 +0200

    Revert "ajout des insert pour la table categorie"
    This reverts commit e3502bf2df460a326dcee0645eaa174fa488fae8.

--- tables.sql
+++ tables.sql
@@ -6,14 +6,3 @@ CREATE TABLE `EQUIPE_CAT` (
   
    
 );
-
-
-insert into categorie values('SENIORS');
-insert into categorie values('U17');
-insert into categorie values('U15');
-insert into categorie values('U14-F');
-insert into categorie values('U13');
-insert into categorie values('U11-F');
-insert into categorie values('U11');
-insert into categorie values('U9');
-insert into categorie values('U7');
