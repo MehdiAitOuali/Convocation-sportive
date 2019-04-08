0ea9d47ad309f35ab3c40c35993f0e194680337b
Ibrahim Noune <ibrahim.noune@etud.univ-angers.fr>
2019-04-08 20:38:16 +0200

    creation de la table arbitrage

--- tables.sql
+++ tables.sql
@@ -3,9 +3,9 @@
 --------------- TABLE CATEGORIE -------------------------------
 
 CREATE TABLE `CATEGORIE` (
-  IDCategorie INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
+  IDCategorie INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   NomCategorie ENUM('SENIORS','U17','U15','U14-F','U13','U11-F','U11','U9','U7')
-   
+
 );
 
 insert into CATEGORIE(NomCategorie) values('SENIORS');
@@ -25,10 +25,10 @@ insert into CATEGORIE(NomCategorie) values('U7');
 
 CREATE TABLE `EQUIPE` (
   IDEquipe INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  NomEquipe VARCHAR(30),	
+  NomEquipe VARCHAR(30),
   IDCategorie INT,
 
-  FOREIGN KEY (IDCategorie) REFERENCES CATEGORIE(IDCategorie)    
+  FOREIGN KEY (IDCategorie) REFERENCES CATEGORIE(IDCategorie)
 );
 
 
@@ -59,46 +59,71 @@ insert into EQUIPE(NomEquipe,IDCategorie) values('U7',9);
 CREATE TABLE `EFFECTIF` (
   IDEffectif INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   NomJoueur VARCHAR(30),
-  PrenomJoueur VARCHAR(30),	
+  PrenomJoueur VARCHAR(30),
   TypeLicence VARCHAR(30),
   IDCategorie INT,
   date_naissance DATE,
   adresse varchar(30),
   mobile varchar(30),
-  email varchar(30),  
+  email varchar(30),
 
-  FOREIGN KEY (IDCategorie) REFERENCES CATEGORIE(IDCategorie)    
+  FOREIGN KEY (IDCategorie) REFERENCES CATEGORIE(IDCategorie)
 );
 
 --------------------- ABSENCE -------------------------------------------
 
 
 CREATE TABLE `ABSENCE` (
-  IDEffectif INT REFERENCES Effectif(IDEffectif),  
+  IDEffectif INT REFERENCES Effectif(IDEffectif),
   DateAbsence DATE,
   Motif enum('Blessé','Non licencié','Suspendé'),
 
-  PRIMARY KEY (IDEffectif,DateAbsence) 
-    
+  PRIMARY KEY (IDEffectif,DateAbsence)
+
 );
 
 --------------------- NON_CONVOC -------------------------------------------
 CREATE TABLE `NON_CONVOC` (
-  IDEffectif INT REFERENCES Effectif(IDEffectif),  
+  IDEffectif INT REFERENCES Effectif(IDEffectif),
   DateC DATE,
   Statut enum('Exempt','Absent'),
 
-  PRIMARY KEY (IDEffectif,DateC) 
-    
+  PRIMARY KEY (IDEffectif,DateC)
+
 );
 
 --------------------- LOGISTIQUE-------------------------------------------
 CREATE TABLE `LOGISTIQUE` (
-  IDEffectif INT REFERENCES Effectif(IDEffectif),  
+  IDEffectif INT REFERENCES Effectif(IDEffectif),
   IDMatch INT REFERENCES MATCHS(IDMatch),
   Tache enum('Maillot','Vestiaires','Buvette','Voiture'),
 
-  PRIMARY KEY (IDEffectif,IDMatch) 
-    
-);
+  PRIMARY KEY (IDEffectif,IDMatch)
 
+);
+--------------------- MATCHS -------------------------------------------
+
+CREATE TABLE `MATCHS` (
+
+  IDMatch INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  IDEquipe INT REFERENCES Equipe(IDEquipe),
+  DateMatch DATETIME,
+  ClubAdverse VARCHAR(50),
+  LocaliteClubAdverse VARCHAR(50),
+  EquipeAdverse VARCHAR(50),
+  Terrain VARCHAR(50),
+  Competition VARCHAR(50),
+  Deplacement enum('Domicile','Extérieur'),
+  Site VARCHAR(30)
+
+);
+--------------------- ARBITRAGE-------------------------------------------
+CREATE TABLE `ARBITRAGE` (
+  IDMatch INT REFERENCES MATCHS(IDMatch),
+  Arbitre_centre varchar(50),
+  Arbitre_touche varchar(50),
+  Dirigeant varchar(50),
+
+  PRIMARY KEY (IDMatch)
+
+);
